df85f70b996e906e9437b7f4ec5396efa78d135d
wictro <viktor@frakton.com>
2022-12-23 14:42:51 +0100

    Pretty Variable Example

---
+++ A Pretty Variable/README.md
@@ -0,0 +1,3 @@
+# A Pretty Variable
+
+Një print në terminal nuk është aq interesante. Në këtë shembull shohim se si vlera e ruajtur në një variabël na paraqitet bukur në një aplikacion IOS.
---
+++ A Pretty Variable/ViewController.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let name:String = "Filan Fisteku"
+        label.text = name
+    }
+}
+
